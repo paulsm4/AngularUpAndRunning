86d3f2138aee5cdd0f389bdbd93253f7f1e7338f
PSantamaria <PSantamaria@pyramidsolutions.com>
2018-12-14 12:44:13 -0800

    Chap2: Added stock-item component, illustrate interpolation

---
+++ chap2/stock-market/src/app/stock/stock-item/stock-item.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-item',
+  templateUrl: './stock-item.component.html',
+  styleUrls: ['./stock-item.component.css']
+})
+export class StockItemComponent implements OnInit {
+
+  public name: string;
+  public code: string;
+  public price: number;
+  public previousPrice: number;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.name = 'Test Stock Company';
+    this.code = 'TSC';
+    this.price = 85;
+    this.previousPrice = 80;
+  }
+
+}
