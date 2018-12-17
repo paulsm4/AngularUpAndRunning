96214d8c1cd976234cbf4653a1d2d0906bd5e425
PSantamaria <PSantamaria@pyramidsolutions.com>
2018-12-17 12:28:09 -0800

    Chap4: Illustrate "Input" (data binding) and "Output" (event binding)

--- chap4/stock-market/src/app/app.component.ts
+++ chap4/stock-market/src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Stock } from './model/stock';
+
+// import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  encapsulation: ViewEncapsulation.None
+  // encapsulation: ViewEncapsulation.None
   // encapsulation: ViewEncapsulation.Native
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Stock Market App';
+
+  public stock: Stock;
+
+  ngOnInit(): void {
+    this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
+  }
+
+  onToggleFavorite(event) {
+    console.log('We are toggling the favorite state of this stock', event);
+    this.stock.favorite = !this.stock.favorite;
+  }
+
 }
--- chap4/stock-market/src/app/stock/stock-item/stock-item.component.ts
+++ chap4/stock-market/src/app/stock/stock-item/stock-item.component.ts
@@ -1,58 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 import { Stock } from '../../model/stock';
 
 @Component({
   selector: 'app-stock-item',
-  // templateUrl: './stock-item.component.html',
-  template: `
-  <div class="stock-container"
-  *ngFor="let stock of stocks; index as i; trackBy: trackStockByCode">
-    <div class="name">{{stock.name + ' (' + stock.code + ')'}}</div>
-    <div class="price"
-        [class]="stock.isPositiveChange() ? 'positive' : 'negative'">
-        $ {{stock.price}}</div>
-    <button (click)="toggleFavorite($event, i)"
-        [disabled]="stock.favorite">Add to Favorite</button>
-  </div>
-  `,
-  // styleUrls: ['./stock-item.component.css' ]
-  styles: [`
-  .stock-container {
-    border: 1px solid black;
-    border-radius: 5px;
-    display: inline-block;
-    padding: 10px;
-  }
-
-  .positive {
-    color: green;
-  }
-
-  .negative {
-    color: red;
-  }
-  `]
+  templateUrl: './stock-item.component.html',
+  styleUrls: ['./stock-item.component.css' ]
 })
-export class StockItemComponent implements OnInit {
 
-  public stocks: Array<Stock>;
+// export class StockItemComponent implements OnInit {
+export class StockItemComponent {
+  @Input() public stock: Stock;
+  @Output() private toggleFavorite: EventEmitter<Stock>;
 
-  constructor() { }
+  constructor() {
+    this.toggleFavorite = new EventEmitter<Stock>();
+   }
 
-  ngOnInit() {
+  /* ngOnInit() {
     this.stocks =[
       new Stock('Test Stock Company', 'TSC', 85, 80),
       new Stock('Second Stock Company', 'SSC', 10, 20),
       new Stock('Last Stock Company', 'LSC', 876, 765)];
-  }
+  } */
 
-  toggleFavorite(event, index) {
-    console.log('We are toggling the favorite state of this stock', index, event);
-    this.stocks[index].favorite = !this.stocks[index].favorite;
+/*   toggleFavorite(event) {
+    console.log('We are toggling the favorite state of this stock', event);
+    this.stock.favorite = !this.stock.favorite;
   }
-
-  trackStockByCode(index, stock) {
-    return stock.code;
+ */
+  onToggleFavorite(event) {
+    this.toggleFavorite.emit(this.stock);
   }
 }
