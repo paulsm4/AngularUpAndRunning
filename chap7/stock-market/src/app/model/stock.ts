57a3ea493b68f1b6c41974d613bd39c59a547796
PSantamaria <PSantamaria@pyramidsolutions.com>
2018-12-20 12:31:28 -0800

    Chap7: Form arrays

--- chap7/stock-market/src/app/model/stock.ts
+++ chap7/stock-market/src/app/model/stock.ts
@@ -1,15 +1,23 @@
 export class Stock {
-  /* tslint:disable */
-  favorite: boolean = false;
-  /* tslint:enable */
+  favorite: boolean;
+  notablePeople : Person[];
 
-  constructor(public name: string,
-              public code: string,
-              public price: number,
-              public previousPrice: number) {}
+  constructor(
+    public name: string,
+    public code: string,
+    public price: number,
+    public previousPrice: number) {
+      this.favorite = false;
+      this.notablePeople = [];
+  }
 
   isPositiveChange(): boolean {
     console.log('Stock::isPositiveChange:', this);
     return this.price >= this.previousPrice;
   }
+}
+
+export class Person {
+  name: string;
+  title: string;
 }
--- chap7/stock-market/src/app/stock/create-stock/create-stock.component.ts
+++ chap7/stock-market/src/app/stock/create-stock/create-stock.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
-import { Stock } from 'src/app/model/stock';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Validators, FormBuilder} from '@angular/forms';
+import { FormArray } from '@angular/forms';
+import { Stock } from '../../model/stock';
 
-/* Note:
-   "counter" is external to "@Component" decorator and "CreateStockComponent" export */
 let counter = 1;
 
 @Component({
@@ -13,33 +13,42 @@ let counter = 1;
 })
 export class CreateStockComponent {
 
-  /*  Manually creating FormGroup:
-  public stockForm: FormGroup = new FormGroup({
-    name: new FormControl(null, Validators.required),
-    code: new FormControl(null, [Validators.required, Validators.minLength(2)]),
-    price: new FormControl(0, [Validators.required, Validators.min(0)])
-  });
-   */
-
-  /*  Substitute FormBuilder for FormControl */
-  private stock;
+  private stock: Stock;
   public stockForm: FormGroup;
+
   constructor(private fb: FormBuilder) {
     this.createForm();
-    this.stock = new Stock('Test ' + counter++, 'TST', 20, 10);
   }
 
   createForm() {
     this.stockForm = this.fb.group({
       name: [null, Validators.required],
       code: [null, [Validators.required, Validators.minLength(2)]],
-      price: [0, [Validators.required, Validators.min(0)]]
+      price: [0, [Validators.required, Validators.min(0)]],
+      notablePeople: this.fb.array([])
     });
   }
 
-  get name() { return this.stockForm.get('name'); }
-  get price() { return this.stockForm.get('price'); }
   get code() { return this.stockForm.get('code'); }
+  get name() { return this.stockForm.get('name'); }
+  /* Note:
+     Need to explictly declare/cast as "FormArray", else other methods
+     using array methods .push() or .removeAt(), etc. won't compile */
+  get notablePeople(): FormArray {
+     return this.stockForm.get('notablePeople') as FormArray;
+  }
+  get price() { return this.stockForm.get('price'); }
+
+  addNotablePerson() {
+    this.notablePeople.push(this.fb.group({
+      name: ['', Validators.required],
+      title: ['', Validators.required]
+    }));
+  }
+
+  removeNotablePerson(index: number) {
+    this.notablePeople.removeAt(index);
+  }
 
   loadStockFromServer() {
     console.log('CreateStockComponent.loadStockFromServer', this.stock);
@@ -49,11 +58,13 @@ export class CreateStockComponent {
     delete stockFormModel.favorite;
     this.stockForm.setValue(stockFormModel);
   }
+
   patchStockForm() {
     console.log('CreateStockComponent.patchStockForm', this.stock);
     this.stock = new Stock('Test3 ' + counter++, 'TST3', 20, 10);
     this.stockForm.patchValue(this.stock);
   }
+
   resetForm() {
     console.log('CreateStockComponent.resetForm', this.stock);
     this.stockForm.reset();
