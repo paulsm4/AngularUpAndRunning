90a5f97bcf945398ac04ee34b7afc5bac4fd957c
PSantamaria <PSantamaria@pyramidsolutions.com>
2018-12-19 21:09:05 -0800

    Chap7: Substitute FormBuilder "syntactic sugar" for creating FormGroup FormControls

--- chap7/stock-market/src/app/stock/create-stock/create-stock.component.ts
+++ chap7/stock-market/src/app/stock/create-stock/create-stock.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-create-stock',
@@ -8,14 +8,27 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class CreateStockComponent {
 
-  public nameControl = new FormControl();
-
+  /*  Manually creating FormGroup:
   public stockForm: FormGroup = new FormGroup({
     name: new FormControl(null, Validators.required),
     code: new FormControl(null, [Validators.required, Validators.minLength(2)]),
     price: new FormControl(0, [Validators.required, Validators.min(0)])
   });
-  constructor() { }
+   */
+
+  /*  Substitute FormBuilder for FormControl */
+  public stockForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.stockForm = this.fb.group({
+      name: [null, Validators.required],
+      code: [null, [Validators.required, Validators.minLength(2)]],
+      price: [0, [Validators.required, Validators.min(0)]]
+    });
+  }
 
   onSubmit() {
     console.log('CreateStockComponent.onSubmit()', this.stockForm.value);
