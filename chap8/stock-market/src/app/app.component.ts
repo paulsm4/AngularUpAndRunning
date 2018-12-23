8ba63945e0a7749b501ebdde2e89e9765d6c23e8
paulsm4 <paulsm@srccorp.com>
2018-12-23 13:38:50 -0800

    Add "MessageService", invoke from two places (app and create-stock)

--- chap8/stock-market/src/app/app.component.ts
+++ chap8/stock-market/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from './services/message.service';
+import { TNodeFlags } from '@angular/core/src/render3/interfaces/node';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,9 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'Stock Market App';
 
+  constructor(public messageService: MessageService) {}
+
   ngOnInit(): void {
+    this.messageService.message = 'Hello MessageService!';
   }
 }
--- chap8/stock-market/src/app/stock/create-stock/create-stock.component.ts
+++ chap8/stock-market/src/app/stock/create-stock/create-stock.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Stock } from '../../model/stock';
 import { StockService } from '../../services/stock.service';
+import { MessageService } from '../../services/message.service';
 
 @Component({
   selector: 'app-create-stock',
   templateUrl: './create-stock.component.html',
-  styleUrls: ['./create-stock.component.css']
+  styleUrls: ['./create-stock.component.css']// ,
+  // providers: [MessageService]
 })
 export class CreateStockComponent {
 
   public stock: Stock;
   public confirmed = false;
-  public message = null;
   public exchanges = ['NYSE', 'NASDAQ', 'OTHER'];
-  constructor(private stockService: StockService) {
+  constructor(
+    private stockService: StockService,
+    public messageService: MessageService) {
     this.stock =  new Stock('', '', 0, 0, 'NASDAQ');
   }
 
@@ -24,12 +27,14 @@ export class CreateStockComponent {
 
   createStock(stockForm) {
     if (stockForm.valid) {
-      let created = this.stockService.createStock(this.stock);
+      const created = this.stockService.createStock(this.stock);
       if (created) {
-        this.message = 'Successfully created stock with stock code: ' + this.stock.code;
+        this.messageService.message = 'Successfully created stock with stock code: ' + this.stock.code;
         this.stock =  new Stock('', '', 0, 0, 'NASDAQ');
+        console.log('createStock@created:', stockForm, this.messageService.message);
       } else {
-        this.message = 'Stock with stock code: ' + this.stock.code + ' already exists';
+        this.messageService.message = 'Stock with stock code: ' + this.stock.code + ' already exists';
+        console.log('createStock@already exists:', stockForm, this.messageService.message);
       }
     } else {
       console.error('Stock form is in an invalid state');
